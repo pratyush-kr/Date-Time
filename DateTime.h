c14e56630a8bd99c9d337496dee7d66f68199a42
pratyush-kr <pratyush151198@gmail.com>
2021-07-17 16:26:49 +0530

    Decluttering

--- DateTime.h
+++ DateTime.h
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<ctime>
 
 #define YEAR_MIN 1000
 #define YEAR_MAX 3000
@@ -15,69 +16,12 @@ class Date
         unsigned int date; //date of month
         unsigned int year; //year
     public:
-        bool check_leap(int year = 0)
-        {
-            if(year == 0)
-                year = this->year;
-            if(year%100 !=0 && year%4 == 0) //non centuary year and divisible by 4
-                return true;
-            else if(year%100 == 0 && year%400 == 0) //centuary year and divisible by 400
-                return true;
-            else
-                return false;
-        }
+        bool check_leap(int);
         Date(const int &, const int &, const int &, const int);//Date, Month, Year
-        Date()
-        {
-            date = 1;
-            month = 1;
-            year = 1970;
-        }
+        Date() {date = month = 1, year = 1970;}
         int dayofweek();
-        void show()
-        {printf("%02d-%02d-%d\n", date, month, year);}
-        friend std::ostream& operator << (std::ostream& out, const Date &d)
-        {
-            std::string str="";
-            std::string months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct" "Nov", "Dec"};
-            std::string days[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-            out<<days[d.day]<<" ";
-            if(d.date%10 == 1)
-            {
-                if(d.date <= 9)
-                    str += '0';
-                str += std::to_string(d.date);
-                out<<str<<"'st ";
-                str = "";
-            }
-            else if(d.date%10 == 2)
-            {
-                if(d.date <= 9)
-                    str += '0';
-                str += std::to_string(d.date);
-                out<<str<<"'nd ";
-                str = "";
-            }
-            else if(d.date%10 == 3)
-            {
-                if(d.date <= 9)
-                    str += '0';
-                str += std::to_string(d.date);
-                out<<str<<"'rd ";
-                str = "";
-            }
-            else
-            {
-                if(d.date <= 9)
-                    str += '0';
-                str += std::to_string(d.date);
-                out<<str<<"'th ";
-                str = "";
-            }
-            out<<months[d.month-1]<<" ";
-            out<<d.year;
-            return out;
-        }
+        void show() {printf("%02d-%02d-%d\n", date, month, year);}
+        friend std::ostream& operator << (std::ostream &, const Date &);
 };
 
 class Time : public Date
@@ -91,30 +35,8 @@ class Time : public Date
         {hrs = h, min = m, sec = s;}
         void show(){printf("%02x:%02x:%02x\n", hrs, min, sec);}
         Time(){hrs = min = sec = 0;}
-        friend std::ostream& operator <<(std::ostream &out, const Time &t)
-        {
-            std::string str = "";
-            if(t.hrs <= 9)
-                str += '0';
-            str += std::to_string(t.hrs);
-            out<<str<<":";
-            str = "";
-            if(t.min <= 9)
-                str += '0';
-            str += std::to_string(t.min);
-            out<<str<<":";
-            str = "";
-            if(t.sec <= 9)
-                str += '0';
-            str += std::to_string(t.sec);
-            out<<str;
-            return out;
-        }
-        friend std::istream& operator >>(std::istream &in, Time &t)
-        {
-            in>>t.hrs>>t.min>>t.sec;
-            return in;
-        }
+        friend std::ostream& operator <<(std::ostream &, const Time &);
+        friend std::istream& operator >>(std::istream &, Time &);
 }pivot;
 
 Date::Date(const int &date, const int &month, 
@@ -156,6 +78,8 @@ Date::Date(const int &date, const int &month,
     this->year = year;
 }
 
+
+
 int Date::dayofweek()
 {
     int d = date, m = month, y = year;
@@ -163,4 +87,85 @@ int Date::dayofweek()
     y -= m<3;
     day = (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
     return day;
+}
+
+std::ostream& operator <<(std::ostream &out, const Time &t)
+{
+    std::string str = "";
+    if(t.hrs <= 9)
+        str += '0';
+    str += std::to_string(t.hrs);
+    out<<str<<":";
+    str = "";
+    if(t.min <= 9)
+        str += '0';
+    str += std::to_string(t.min);
+    out<<str<<":";
+    str = "";
+    if(t.sec <= 9)
+        str += '0';
+    str += std::to_string(t.sec);
+    out<<str;
+    return out;
+}
+
+std::istream& operator >>(std::istream &in, Time &t)
+{
+    in>>t.hrs>>t.min>>t.sec;
+    return in;
+}
+
+std::ostream& operator << (std::ostream& out, const Date &d)
+{
+    std::string str="";
+    std::string months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct" "Nov", "Dec"};
+    std::string days[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    out<<days[d.day]<<" ";
+    if(d.date%10 == 1)
+    {
+        if(d.date <= 9)
+            str += '0';
+        str += std::to_string(d.date);
+        out<<str<<"st ";
+        str = "";
+    }
+    else if(d.date%10 == 2)
+    {
+        if(d.date <= 9)
+            str += '0';
+        str += std::to_string(d.date);
+        out<<str<<"'nd ";
+        str = "";
+    }
+    else if(d.date%10 == 3)
+    {
+        if(d.date <= 9)
+            str += '0';
+        str += std::to_string(d.date);
+        out<<str<<"'rd ";
+        str = "";
+    }
+    else
+    {
+        if(d.date <= 9)
+            str += '0';
+        str += std::to_string(d.date);
+        out<<str<<"'th ";
+        str = "";
+    }
+    out<<months[d.month-1]<<" ";
+    out<<d.year;
+    return out;
+}
+
+bool Date::check_leap(int year = 0)
+{
+    if(year == 0)
+        year = this->year;
+    if(year%100 !=0 && year%4 == 0) //non centuary year and divisible by 4
+        return true;
+    else if(year%100 == 0 && year%400 == 0) //centuary year and divisible by 400
+        return true;
+    else
+        return false;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include "DateTime.h"
+#include<iostream>
+
+int main()
+{
+    Date d(15, 11, 1999);
+    d.show();
+    Time t(9, 52, 0);
